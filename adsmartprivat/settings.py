61de79737067caf810b5907a4a1cc02898198e39
Aswindanu1437 <aswindanu@alterra.id>
2020-07-23 17:33:39 +0700

    bug static

--- adsmartprivat/settings.py
+++ adsmartprivat/settings.py
@@ -167,7 +167,7 @@ MEDIA_ROOT = (
 
 # Extra lookup directories for collectstatic to find static files
 STATICFILES_DIRS = (
-    os.path.join(PROJECT_ROOT, '../../home/static/uploads/static'),
+    os.path.join(PROJECT_ROOT, '../../uploads'),
 )
 
 #  Add configuration for static files storage using whitenoise
