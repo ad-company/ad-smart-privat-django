645de88259804857328e8e7d193d040716ba343c
Aswindanu1437 <aswindanu@alterra.id>
2020-09-04 16:28:26 +0700

    Update initial

--- heroku-shell.sh
+++ heroku-shell.sh
@@ -1,2 +1,2 @@
+echo "URUTAN : 1. Push, makemigration, and migrate; 2. Update 0001_initial.py manually; 3. Push, makemigrations, and migrate (just for check) "
 heroku vim
-echo "URUTAN : 1. Push, makemigration, and migrate; 2. Update 0001_initial.py manually; 3. Push, makemigrations, and migrate "
